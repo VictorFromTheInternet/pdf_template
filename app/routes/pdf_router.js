b4dba523e1b15376636001c462b345acaa291da3
VictorFromTheInternet <vma0430@gmail.com>
2025-02-25 21:09:32 -0600

    adding puppeteer pdf generation to the demo route

--- app/routes/pdf_router.js
+++ app/routes/pdf_router.js
@@ -1,13 +1,37 @@
 const express = require('express')
+const puppeteer = require('puppeteer')
+const fs = require('fs-extra')
+
 const router = express.Router()
 
 router.get('/',(req,res)=>{
     res.send({"message":"hello world"})
 })
 
-router.post('/demo',(req,res)=>{
+router.post('/demo', async (req,res)=>{
+    try{
+        const browser = await puppeteer.launch()     
+        const page = await browser.newPage()
+        
+        await page.setContent('<h1> Hello World </h1>')
+        await page.emulateMediaType('screen')
+        await page.pdf({
+            path: 'test.pdf',
+            format: 'A4',
+            printBackground: true
+        })
+
+        console.log('Generated PDF ')
+        await browser.close()
+
+        res.send({"message":"Generated PDF"})
+
+
+
+    }catch(err){
+        console.log(err)
+    }
     
-    res.send(req.body)
 })
 
 module.exports = router
